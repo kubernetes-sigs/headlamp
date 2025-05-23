1b342b7383b29d2e7c5b55f09fc92625d8d06809
Jaishree2310 <jaishrees23102001@gmail.com>
2025-05-23 23:07:44 +0530

    fix: resolve linter issues  by refactoring TestParse
    Signed-off-by: Jaishree2310 <jaishrees23102001@gmail.com>

--- backend/pkg/config/config_test.go
+++ backend/pkg/config/config_test.go
@@ -1,147 +1,167 @@
 package config_test
 
 import (
-	"os"
-	"testing"
+    "os"
+    "testing"
 
-	"github.com/kubernetes-sigs/headlamp/backend/pkg/config"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+    "github.com/kubernetes-sigs/headlamp/backend/pkg/config"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
 )
 
 type testCase struct {
-	name     string
-	args     []string
-	envVars  map[string]string
-	validate func(t *testing.T, conf *config.Config, err error)
+    name string
+    args[] string
+    envVars map[string] string
+    validate func(t * testing.T, conf * config.Config, err error)
 }
 
-func setEnvVars(envVars map[string]string) func() {
-	originals := make(map[string]string, len(envVars))
-
-	for k := range envVars {
-		originals[k] = os.Getenv(k)
-		os.Setenv(k, envVars[k])
-	}
-
-	return func() {
-		for k, v := range originals {
-			if v == "" {
-				os.Unsetenv(k)
-			} else {
-				os.Setenv(k, v)
-			}
-		}
-	}
+func setEnvVars(envVars map[string] string) func() {
+    originals: = make(map[string] string, len(envVars))
+    for k: = range envVars {
+        originals[k] = os.Getenv(k)
+        os.Setenv(k, envVars[k])
+    }
+    return func() {
+        for k, v: = range originals {
+            if v == "" {
+                os.Unsetenv(k)
+            } else {
+                os.Setenv(k, v)
+            }
+        }
+    }
 }
 
-func TestParse(t *testing.T) {
-	tests := []testCase{
-		{
-			name: "no_args_no_env",
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.NoError(t, err)
-				require.NotNil(t, conf)
-				assert.Equal(t, false, conf.DevMode)
-				assert.Equal(t, "", conf.ListenAddr)
-				assert.Equal(t, uint(4466), conf.Port)
-				assert.Equal(t, "profile,email", conf.OidcScopes)
-			},
-		},
-		{
-			name: "with_args",
-			args: []string{"go run ./cmd", "--port=3456"},
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.NoError(t, err)
-				require.NotNil(t, conf)
-				assert.Equal(t, uint(3456), conf.Port)
-			},
-		},
-		{
-			name: "from_env",
-			args: []string{"go run ./cmd", "-in-cluster"},
-			envVars: map[string]string{
-				"HEADLAMP_CONFIG_OIDC_CLIENT_SECRET": "superSecretBotsStayAwayPlease",
-			},
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.NoError(t, err)
-				require.NotNil(t, conf)
-				assert.Equal(t, "superSecretBotsStayAwayPlease", conf.OidcClientSecret)
-			},
-		},
-		{
-			name: "both_args_and_env",
-			args: []string{"go run ./cmd", "--port=9876"},
-			envVars: map[string]string{
-				"HEADLAMP_CONFIG_PORT": "1234",
-			},
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.NoError(t, err)
-				require.NotNil(t, conf)
-				assert.NotEqual(t, uint(1234), conf.Port)
-				assert.Equal(t, uint(9876), conf.Port)
-			},
-		},
-		{
-			name: "oidc_settings_without_incluster",
-			args: []string{"go run ./cmd", "-oidc-client-id=noClient"},
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.Error(t, err)
-				require.Nil(t, conf)
-				assert.Contains(t, err.Error(), "are only meant to be used in inCluster mode")
-			},
-		},
-		{
-			name: "invalid_base_url",
-			args: []string{"go run ./cmd", "--base-url=testingthis"},
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.Error(t, err)
-				require.Nil(t, conf)
-				assert.Contains(t, err.Error(), "base-url")
-			},
-		},
-		{
-			name: "kubeconfig_from_default_env",
-			args: []string{"go run ./cmd"},
-			envVars: map[string]string{
-				"KUBECONFIG": "~/.kube/test_config.yaml",
-			},
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.NoError(t, err)
-				require.NotNil(t, conf)
-				assert.Equal(t, "~/.kube/test_config.yaml", conf.KubeConfigPath)
-			},
-		},
-		{
-			name: "enable_dynamic_clusters",
-			args: []string{"go run ./cmd", "--enable-dynamic-clusters"},
-			validate: func(t *testing.T, conf *config.Config, err error) {
-				require.NoError(t, err)
-				require.NotNil(t, conf)
-				assert.Equal(t, true, conf.EnableDynamicClusters)
-			},
-		},
-	}
+func validateNoArgsNoEnv(t * testing.T, conf * config.Config, err error) {
+    require.NoError(t, err)
+    require.NotNil(t, conf)
+    assert.Equal(t, false, conf.DevMode)
+    assert.Equal(t, "", conf.ListenAddr)
+    assert.Equal(t, uint(4466), conf.Port)
+    assert.Equal(t, "profile,email", conf.OidcScopes)
+}
 
-	for _, tc := range tests {
-		tc := tc
+func validateWithArgs(t * testing.T, conf * config.Config, err error) {
+    require.NoError(t, err)
+    require.NotNil(t, conf)
+    assert.Equal(t, uint(3456), conf.Port)
+}
 
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
+func validateFromEnv(t * testing.T, conf * config.Config, err error) {
+    require.NoError(t, err)
+    require.NotNil(t, conf)
+    assert.Equal(t, "superSecretBotsStayAwayPlease", conf.OidcClientSecret)
+}
 
-			var cleanup func()
+func validateBothArgsAndEnv(t * testing.T, conf * config.Config, err error) {
+    require.NoError(t, err)
+    require.NotNil(t, conf)
+    assert.NotEqual(t, uint(1234), conf.Port)
+    assert.Equal(t, uint(9876), conf.Port)
+}
 
-			if len(tc.envVars) > 0 {
-				cleanup = setEnvVars(tc.envVars)
-			}
+func validateOidcSettingsWithoutIncluster(t * testing.T, conf * config.Config, err error) {
+    require.Error(t, err)
+    require.Nil(t, conf)
+    assert.Contains(t, err.Error(), "are only meant to be used in inCluster mode")
+}
 
-			if cleanup != nil {
-				defer cleanup()
-			}
+func validateInvalidBaseURL(t * testing.T, conf * config.Config, err error) {
+    require.Error(t, err)
+    require.Nil(t, conf)
+    assert.Contains(t, err.Error(), "base-url")
+}
 
-			conf, err := config.Parse(tc.args)
+func validateKubeconfigFromDefaultEnv(t * testing.T, conf * config.Config, err error) {
+    require.NoError(t, err)
+    require.NotNil(t, conf)
+    assert.Equal(t, "~/.kube/test_config.yaml", conf.KubeConfigPath)
+}
 
-			tc.validate(t, conf, err)
-		})
-	}
+func validateEnableDynamicClusters(t * testing.T, conf * config.Config, err error) {
+    require.NoError(t, err)
+    require.NotNil(t, conf)
+    assert.Equal(t, true, conf.EnableDynamicClusters)
+}
+
+func getTestCases()[] testCase {
+    return [] testCase {
+        {
+            name: "no_args_no_env",
+            validate: validateNoArgsNoEnv,
+        }, {
+            name: "with_args",
+            args: [] string {
+                "go run ./cmd", "--port=3456"
+            },
+            validate: validateWithArgs,
+        }, {
+            name: "from_env",
+            args: [] string {
+                "go run ./cmd", "-in-cluster"
+            },
+            envVars: map[string] string {
+                "HEADLAMP_CONFIG_OIDC_CLIENT_SECRET": "superSecretBotsStayAwayPlease",
+            },
+            validate: validateFromEnv,
+        }, {
+            name: "both_args_and_env",
+            args: [] string {
+                "go run ./cmd", "--port=9876"
+            },
+            envVars: map[string] string {
+                "HEADLAMP_CONFIG_PORT": "1234",
+            },
+            validate: validateBothArgsAndEnv,
+        }, {
+            name: "oidc_settings_without_incluster",
+            args: [] string {
+                "go run ./cmd", "-oidc-client-id=noClient"
+            },
+            validate: validateOidcSettingsWithoutIncluster,
+        }, {
+            name: "invalid_base_url",
+            args: [] string {
+                "go run ./cmd", "--base-url=testingthis"
+            },
+            validate: validateInvalidBaseURL,
+        }, {
+            name: "kubeconfig_from_default_env",
+            args: [] string {
+                "go run ./cmd"
+            },
+            envVars: map[string] string {
+                "KUBECONFIG": "~/.kube/test_config.yaml",
+            },
+            validate: validateKubeconfigFromDefaultEnv,
+        }, {
+            name: "enable_dynamic_clusters",
+            args: [] string {
+                "go run ./cmd", "--enable-dynamic-clusters"
+            },
+            validate: validateEnableDynamicClusters,
+        },
+    }
+}
+
+func TestParse(t * testing.T) {
+    tests: = getTestCases()
+
+    for _,
+    tc: = range tests {
+        tc: = tc
+        t.Run(tc.name, func(t * testing.T) {
+            t.Parallel()
+            var cleanup func()
+            if len(tc.envVars) > 0 {
+                cleanup = setEnvVars(tc.envVars)
+            }
+            if cleanup != nil {
+                defer cleanup()
+            }
+            conf, err: = config.Parse(tc.args)
+            tc.validate(t, conf, err)
+        })
+    }
 }
