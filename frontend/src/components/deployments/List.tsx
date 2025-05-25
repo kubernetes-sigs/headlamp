e0b66b26ddd9fc6b77f7a50ccd781029c1e5fe82
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-05-25 12:35:29 +0545

    frontend: Table: Add Global filter

--- frontend/src/components/deployments/List.tsx
+++ frontend/src/components/deployments/List.tsx
@@ -77,6 +77,7 @@ export default function DeploymentsList() {
     <ResourceListView
       title={t('Deployments')}
       resourceClass={Deployment}
+      reflectInURL
       columns={[
         'name',
         'namespace',
