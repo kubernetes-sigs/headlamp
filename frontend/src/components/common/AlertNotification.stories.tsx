a4e6ff5dccffaf313dc110198f44a8694e1e6eb4
ANIRUDH-SJ <anirudhsj2004@gmail.com>
2025-05-27 20:30:07 +0530

    frontend: AlertNotification: update storybook file

--- frontend/src/components/common/AlertNotification.stories.tsx
+++ frontend/src/components/common/AlertNotification.stories.tsx
@@ -179,38 +179,3 @@ OnNonClusterRoute.args = {
   simulateSnackbar: { show: false, message: '', variant: 'info' },
 };
 OnNonClusterRoute.storyName = 'On Non-Cluster Route (e.g., Global Settings)';
-
-//----
-// import { Meta, StoryFn } from '@storybook/react';
-// import { SnackbarProvider } from 'notistack';
-// import { TestContext } from '../../test';
-// import { PureAlertNotification, PureAlertNotificationProps } from './AlertNotification';
-
-// export default {
-//   title: 'AlertNotification',
-//   component: PureAlertNotification,
-//   argTypes: {
-//     dispatch: { action: 'dispatch' },
-//   },
-//   decorators: [
-//     Story => (
-//       <TestContext>
-//         <SnackbarProvider>
-//           <Story />
-//         </SnackbarProvider>
-//       </TestContext>
-//     ),
-//   ],
-// } as Meta;
-
-// const Template: StoryFn<PureAlertNotificationProps> = args => <PureAlertNotification {...args} />;
-
-// export const Error = Template.bind({});
-// Error.args = {
-//   checkerFunction: () => Promise.reject('offline'),
-// };
-
-// export const NoError = Template.bind({});
-// NoError.args = {
-//   checkerFunction: () => Promise.resolve({ statusText: 'OK' }),
-// };
