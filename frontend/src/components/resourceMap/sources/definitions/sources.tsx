bdc488223d1c5d15175912cdb55cfcec71724f02
Siddharth Bansal <sidd190bansal@gmail.com>
2025-06-18 21:37:38 +0530

    Formatted the file with Prettier

--- frontend/src/components/resourceMap/sources/definitions/sources.tsx
+++ frontend/src/components/resourceMap/sources/definitions/sources.tsx
@@ -14,34 +14,34 @@
  * limitations under the License.
  */
 
-import { Icon } from '@iconify/react';
-import { useMemo } from 'react';
-import ConfigMap from '../../../../lib/k8s/configMap';
-import CronJob from '../../../../lib/k8s/cronJob';
-import DaemonSet from '../../../../lib/k8s/daemonSet';
-import Deployment from '../../../../lib/k8s/deployment';
-import Endpoints from '../../../../lib/k8s/endpoints';
-import HPA from '../../../../lib/k8s/hpa';
-import Ingress from '../../../../lib/k8s/ingress';
-import IngressClass from '../../../../lib/k8s/ingressClass';
-import Job from '../../../../lib/k8s/job';
-import { KubeObjectClass } from '../../../../lib/k8s/KubeObject';
-import MutatingWebhookConfiguration from '../../../../lib/k8s/mutatingWebhookConfiguration';
-import NetworkPolicy from '../../../../lib/k8s/networkpolicy';
-import PersistentVolumeClaim from '../../../../lib/k8s/persistentVolumeClaim';
-import Pod from '../../../../lib/k8s/pod';
-import ReplicaSet from '../../../../lib/k8s/replicaSet';
-import Role from '../../../../lib/k8s/role';
-import RoleBinding from '../../../../lib/k8s/roleBinding';
-import Secret from '../../../../lib/k8s/secret';
-import Service from '../../../../lib/k8s/service';
-import ServiceAccount from '../../../../lib/k8s/serviceAccount';
-import StatefulSet from '../../../../lib/k8s/statefulSet';
-import ValidatingWebhookConfiguration from '../../../../lib/k8s/validatingWebhookConfiguration';
-import { useNamespaces } from '../../../../redux/filterSlice';
-import { GraphSource } from '../../graph/graphModel';
-import { getKindGroupColor, KubeIcon } from '../../kubeIcon/KubeIcon';
-import { makeKubeObjectNode } from '../GraphSources';
+import { Icon } from "@iconify/react";
+import { useMemo } from "react";
+import ConfigMap from "../../../../lib/k8s/configMap";
+import CronJob from "../../../../lib/k8s/cronJob";
+import DaemonSet from "../../../../lib/k8s/daemonSet";
+import Deployment from "../../../../lib/k8s/deployment";
+import Endpoints from "../../../../lib/k8s/endpoints";
+import HPA from "../../../../lib/k8s/hpa";
+import Ingress from "../../../../lib/k8s/ingress";
+import IngressClass from "../../../../lib/k8s/ingressClass";
+import Job from "../../../../lib/k8s/job";
+import { KubeObjectClass } from "../../../../lib/k8s/KubeObject";
+import MutatingWebhookConfiguration from "../../../../lib/k8s/mutatingWebhookConfiguration";
+import NetworkPolicy from "../../../../lib/k8s/networkpolicy";
+import PersistentVolumeClaim from "../../../../lib/k8s/persistentVolumeClaim";
+import Pod from "../../../../lib/k8s/pod";
+import ReplicaSet from "../../../../lib/k8s/replicaSet";
+import Role from "../../../../lib/k8s/role";
+import RoleBinding from "../../../../lib/k8s/roleBinding";
+import Secret from "../../../../lib/k8s/secret";
+import Service from "../../../../lib/k8s/service";
+import ServiceAccount from "../../../../lib/k8s/serviceAccount";
+import StatefulSet from "../../../../lib/k8s/statefulSet";
+import ValidatingWebhookConfiguration from "../../../../lib/k8s/validatingWebhookConfiguration";
+import { useNamespaces } from "../../../../redux/filterSlice";
+import { GraphSource } from "../../graph/graphModel";
+import { getKindGroupColor, KubeIcon } from "../../kubeIcon/KubeIcon";
+import { makeKubeObjectNode } from "../GraphSources";
 
 /**
  * Create a GraphSource from KubeObject class definition
@@ -53,20 +53,23 @@ const makeKubeSource = (cl: KubeObjectClass): GraphSource => ({
   useData() {
     const [items] = cl.useList({ namespace: useNamespaces() });
 
-    return useMemo(() => (items ? { nodes: items?.map(makeKubeObjectNode) } : null), [items]);
+    return useMemo(
+      () => (items ? { nodes: items?.map(makeKubeObjectNode) } : null),
+      [items],
+    );
   },
 });
 
 export const allSources: GraphSource[] = [
   {
-    id: 'workloads',
-    label: 'Workloads',
+    id: "workloads",
+    label: "Workloads",
     icon: (
       <Icon
         icon="mdi:circle-slice-2"
         width="100%"
         height="100%"
-        color={getKindGroupColor('workloads')}
+        color={getKindGroupColor("workloads")}
       />
     ),
     sources: [
@@ -80,17 +83,29 @@ export const allSources: GraphSource[] = [
     ],
   },
   {
-    id: 'storage',
-    label: 'Storage',
+    id: "storage",
+    label: "Storage",
     icon: (
-      <Icon icon="mdi:database" width="100%" height="100%" color={getKindGroupColor('storage')} />
+      <Icon
+        icon="mdi:database"
+        width="100%"
+        height="100%"
+        color={getKindGroupColor("storage")}
+      />
     ),
     sources: [makeKubeSource(PersistentVolumeClaim)],
   },
   {
-    id: 'network',
-    label: 'Network',
-    icon: <Icon icon="mdi:folder-network-online" width="100%" height="100%" color={getKindGroupColor('network')} />,
+    id: "network",
+    label: "Network",
+    icon: (
+      <Icon
+        icon="mdi:folder-network-online"
+        width="100%"
+        height="100%"
+        color={getKindGroupColor("network")}
+      />
+    ),
     sources: [
       makeKubeSource(Service),
       makeKubeSource(Endpoints),
@@ -100,21 +115,32 @@ export const allSources: GraphSource[] = [
     ],
   },
   {
-    id: 'security',
-    label: 'Security',
+    id: "security",
+    label: "Security",
     isEnabledByDefault: false,
-    icon: <Icon icon="mdi:lock" width="100%" height="100%" color={getKindGroupColor('security')} />,
-    sources: [makeKubeSource(ServiceAccount), makeKubeSource(Role), makeKubeSource(RoleBinding)],
+    icon: (
+      <Icon
+        icon="mdi:lock"
+        width="100%"
+        height="100%"
+        color={getKindGroupColor("security")}
+      />
+    ),
+    sources: [
+      makeKubeSource(ServiceAccount),
+      makeKubeSource(Role),
+      makeKubeSource(RoleBinding),
+    ],
   },
   {
-    id: 'configuration',
-    label: 'Configuration',
+    id: "configuration",
+    label: "Configuration",
     icon: (
       <Icon
         icon="mdi:format-list-checks"
         width="100%"
         height="100%"
-        color={getKindGroupColor('configuration')}
+        color={getKindGroupColor("configuration")}
       />
     ),
     isEnabledByDefault: false,
