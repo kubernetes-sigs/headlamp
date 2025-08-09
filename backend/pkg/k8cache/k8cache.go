200734272641a60481207ceefdfb3ca6ffc5fff4
Saurav Upadhyay <sauravup041103@gmail.com>
2025-08-09 05:52:21 +0000

    backend: k8cache/k8cache.go: Add responseCapture struct for storing the response from K8s clusters
    This help to capture the response that is coming from a K8s cluster
    responseCapture struct has properties like statusCode, Header and Body.
    
    statusCode: It is the status code (200, 404, 400 ) that will help to identify if the requests was successful  or not.
    Headers: It is use to understand about the nature of the header that is received from the k8 clusters which can be
    returned to the  client.
    Body: Body is the value that will be stored in the cache ( key-value pair )

---
+++ backend/pkg/k8cache/k8cache.go
@@ -0,0 +1,38 @@
+// Copyright 2025 The Kubernetes Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package k8cache
+
+import (
+	"bytes"
+	"net/http"
+)
+
+// CachedResponseData is a struct that will capture statusCode, Headers and Body
+// while the response is coming from the K8s clusters.
+type responseCapture struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       *bytes.Buffer
+}
+
+// WriteHeader write the headers that to the Headers property in CachedResponseData struct.
+func (r *responseCapture) WriteHeader(code int) {
+	r.StatusCode = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
+// Write help to write the body of the response data into the CacheResponseData struct.
+func (r *responseCapture) Write(b []byte) (int, error) {
+	r.Body.Write(b)
+	return r.ResponseWriter.Write(b)
+}
