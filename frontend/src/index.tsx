a2b0d54ccd1e608f013450330a51c7dced4cce3f
Daniel Leone <mail.dleone@gmail.com>
2025-08-22 18:07:41 +0800

    change from loading via ESM to loading pre-transpiled AMD modules - this allows the localization setting to work, as the web worker will fetch the appropriate nls file for the configured language. I could not figure out how to get the workers to load different languages when loading the ESM module

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -18,9 +18,6 @@ import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { initializeMonacoEditor } from './helpers/initializeMonacoEditor';
-
-initializeMonacoEditor();
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
