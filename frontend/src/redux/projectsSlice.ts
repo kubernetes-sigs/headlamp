76aab563f49c0ea077edac3e707988465ef3c58a
Joaquim Rocha <joaquim.rocha@microsoft.com>
2025-08-28 18:45:48 +0100

    frontend: Allow overriding default tabs in the project view
    Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

--- frontend/src/redux/projectsSlice.ts
+++ frontend/src/redux/projectsSlice.ts
@@ -48,9 +48,11 @@ export interface ProjectOverviewSection {
 
 export interface ProjectDetailsTab {
   id: string;
-  label: string;
+  label?: string;
   icon: string | ReactNode;
-  component: (props: { project: ProjectDefinition; projectResources: KubeObject[] }) => ReactNode;
+  component:
+    | ((props: { project: ProjectDefinition; projectResources: KubeObject[] }) => ReactNode)
+    | null;
 }
 
 export interface ProjectsState {
