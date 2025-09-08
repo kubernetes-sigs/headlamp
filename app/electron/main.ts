8c0a37c547ae719769c0d02d13f28f9f4ede0fb5
Ashu Ghildiyal <aghildiyal@microsoft.com>
2025-09-08 22:54:15 +0530

    app: Add mcp client

--- app/electron/main.ts
+++ app/electron/main.ts
@@ -40,6 +40,7 @@ import url from 'url';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import i18n from './i18next.config';
+import ElectronMCPClient from './mcp-client';
 import {
   addToPath,
   ArtifactHubHeadlampPkg,
@@ -131,6 +132,7 @@ const shouldCheckForUpdates = process.env.HEADLAMP_CHECK_FOR_UPDATES !== 'false'
 
 // make it global so that it doesn't get garbage collected
 let mainWindow: BrowserWindow | null;
+let mcpClient: ElectronMCPClient;
 
 /**
  * `Action` is an interface for an action to be performed by the plugin manager.
@@ -1466,6 +1468,10 @@ function startElecron() {
     i18n.off('languageChanged');
     if (mainWindow) {
       mainWindow.removeAllListeners('close');
+    }
+    // Cleanup MCP client
+    if (mcpClient) {
+      mcpClient.cleanup().catch(console.error);
     }
   });
 }
