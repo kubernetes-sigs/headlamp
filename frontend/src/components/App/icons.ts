99affdabbd81fec8eb88246e736dc33b43883179
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-12-01 11:18:32 -0500

    frontend: icons: Add chevron-up icon

--- frontend/src/components/App/icons.ts
+++ frontend/src/components/App/icons.ts
@@ -111,6 +111,9 @@ const mdiIcons = {
     'shield-alert': {
       body: '\u003Cpath fill="currentColor" d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5M11 7h2v6h-2m0 2h2v2h-2"/\u003E',
     },
+    'chevron-up': {
+      body: '\u003Cpath fill="currentColor" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6l-6 6z"/\u003E',
+    },
     'shield-key': {
       body: '\u003Cpath fill="currentColor" d="M12 8a1 1 0 0 1 1 1a1 1 0 0 1-1 1a1 1 0 0 1-1-1a1 1 0 0 1 1-1m9 3c0 5.55-3.84 10.74-9 12c-5.16-1.26-9-6.45-9-12V5l9-4l9 4zm-9-5a3 3 0 0 0-3 3c0 1.31.83 2.42 2 2.83V18h2v-2h2v-2h-2v-2.17c1.17-.41 2-1.52 2-2.83a3 3 0 0 0-3-3"/\u003E',
     },
