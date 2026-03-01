6072cee508c47076a5b52d7cf5e6d34df21636f6
Kevin Hannon <kehannon@redhat.com>
2026-03-01 15:43:22 -0500

    frontend: Add JobSet as a first-class workload resource
    Add support for the JobSet CRD (jobset.x-k8s.io/v1alpha2) throughout
    the frontend, including class definition, list/detail views, sidebar
    entry, workload overview, global search, resource map, and routing.

---
+++ frontend/src/components/jobset/List.tsx
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2025 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useTranslation } from 'react-i18next';
+import JobSet from '../../lib/k8s/jobSet';
+import { useNamespaces } from '../../redux/filterSlice';
+import ResourceListView from '../common/Resource/ResourceListView';
+
+export default function JobSetList() {
+  const { items: jobSets, errors } = JobSet.useList({ namespace: useNamespaces() });
+  const { t } = useTranslation(['glossary', 'translation']);
+
+  return (
+    <ResourceListView
+      title={t('glossary|Job Sets')}
+      errors={errors}
+      columns={[
+        'name',
+        'namespace',
+        'cluster',
+        {
+          id: 'conditions',
+          label: t('translation|Conditions'),
+          gridTemplate: 'min-content',
+          getValue: (jobSet: JobSet) => {
+            const conditions = jobSet.status?.conditions;
+            if (!conditions) return '-';
+            const condition = conditions.find(c => c.status === 'True');
+            return condition?.type ?? '-';
+          },
+        },
+        'age',
+      ]}
+      data={jobSets}
+      reflectInURL="jobsets"
+      id="headlamp-jobsets"
+    />
+  );
+}
