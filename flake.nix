32fe35e2d92d31edc154580a93eb7d7d5cb91e66
r0chd <100892812+unixpariah@users.noreply.github.com>
2026-04-01 13:17:42 +0200

    add nix devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = builtins.attrValues {
+            inherit (pkgs)
+              go
+              nodejs
+              ;
+          };
+        };
+      });
+    };
+}
