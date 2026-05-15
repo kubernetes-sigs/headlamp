51befcc334a714d32aad3312b0c533fc082cb181
PrakharJain345 <jkavita357@gmail.com>
2026-05-15 19:00:24 +0530

    backend: tests: Use SystemRoot for PowerShell path

--- backend/pkg/exec/testdata/test-plugin.bat
+++ backend/pkg/exec/testdata/test-plugin.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 if defined KUBERNETES_EXEC_INFO echo !KUBERNETES_EXEC_INFO! 1>&2
 if defined TEST_OUTPUT_B64 (
-  C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -Command "[Console]::Out.Write([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($env:TEST_OUTPUT_B64)))"
+  %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -Command "[Console]::Out.Write([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($env:TEST_OUTPUT_B64)))"
 ) else (
   echo !TEST_OUTPUT!
 )
