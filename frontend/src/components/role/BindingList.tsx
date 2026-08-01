ade01575691db1460157b21f3d230894252da770
Yashvant Singh <23bee122@nith.ac.in>
2026-08-01 19:22:29 +0530

    frontend: BindingList: Show service account namespace when it differs

--- frontend/src/components/role/BindingList.tsx
+++ frontend/src/components/role/BindingList.tsx
@@ -165,14 +165,22 @@ export default function RoleBindingList() {
           getValue: item =>
             item?.subjects
               ?.filter(subject => subject.kind === 'ServiceAccount')
-              ?.map(subject => subject.name)
+              ?.map(subject =>
+                subject.namespace && subject.namespace !== item.getNamespace()
+                  ? `${subject.name} (${subject.namespace})`
+                  : subject.name
+              )
               ?.join(' '),
           render: item => (
             <LabelListItem
               labels={
                 item?.subjects
                   ?.filter(subject => subject.kind === 'ServiceAccount')
-                  .map(subject => subject.name) || []
+                  .map(subject =>
+                    subject.namespace && subject.namespace !== item.getNamespace()
+                      ? `${subject.name} (${subject.namespace})`
+                      : subject.name
+                  ) || []
               }
             />
           ),
