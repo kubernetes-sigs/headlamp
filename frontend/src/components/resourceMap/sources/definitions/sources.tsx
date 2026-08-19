bb913c738c919c66d5bc81962e152737e650a59b
preetham <preethampujari395@gmail.com>
2026-08-19 20:01:07 +0530

    frontend: Fix useMemo deps for tlsRouteEnabled

--- frontend/src/components/resourceMap/sources/definitions/sources.tsx
+++ frontend/src/components/resourceMap/sources/definitions/sources.tsx
@@ -362,5 +362,13 @@ export function useGetAllSources(): GraphSource[] {
     }
 
     return sources;
-  }, [CustomResourceDefinition, vpaEnabled, gatewayEnabled, tcpRouteEnabled, udpRouteEnabled, t]);
+  }, [
+    CustomResourceDefinition,
+    vpaEnabled,
+    gatewayEnabled,
+    tcpRouteEnabled,
+    tlsRouteEnabled,
+    udpRouteEnabled,
+    t,
+  ]);
 }
